5c431869953de91764983be3bcdc4f932abb08cf
OlegKarasiov <karasiovoleg@gmail.com>
2024-05-18 20:32:19 +0300

    bug: minor fix

--- MindfulnessApi/Models/QuestionItem.cs
+++ MindfulnessApi/Models/QuestionItem.cs
@@ -2,8 +2,6 @@ namespace MindfulnessApi.Models
 {
     public class QuestionItem
     {
-        public int Id { get; set; }
-
         public string Answer { get; set; }
 
         public string Question { get; set; }
